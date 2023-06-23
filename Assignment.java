1262cc6dc5dbfa14224e5b7d0e6b3515872b7cf0
GOWTHAM AKULA <93877342+AkulaGowtham@users.noreply.github.com>
2023-06-23 14:34:27 +0530

    Create Assignment.java

---
+++ Assignment.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Assignment {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the name of the company:");
+        String name = sc.nextLine();
+        System.out.print("Enter the Loc of the company:");
+        String location = sc.nextLine();
+        System.out.println(name+" "+"Technologies"+" "+location);
+        
+    }
+}
